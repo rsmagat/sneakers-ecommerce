bc8525293c85eac46314d8904b7fce17b4992fb9
Remelson <rsmagatca@outlook.com>
2021-11-15 17:22:47 -0600

    Updated Sneakers view and controller

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -2,4 +2,9 @@ class SneakersController < ApplicationController
   def index
     @sneakers = Sneaker.all
   end
+
+  def show
+    @sneakers = Sneaker.all
+    @sneaker = Sneaker.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root "sneakers#index"
+    get "/sneakers/:id", to: "sneakers#show", as: "sneaker"
     get "/brands", to: "brands#index", as: "brands"
     get "/brands/:id", to: "brands#show", as: "brand"
 end
