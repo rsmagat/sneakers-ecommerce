99fb042e9938b2b591ed2660fd0707147b1a577c
Remelson <rsmagatca@outlook.com>
2021-11-15 18:55:41 -0600

    Updated the styling and hyperlinks of Sneakers and Brands

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root "sneakers#index"
+    get "/sneakers", to: "sneakers#index", as: "sneakers"
     get "/sneakers/:id", to: "sneakers#show", as: "sneaker"
     get "/brands", to: "brands#index", as: "brands"
     get "/brands/:id", to: "brands#show", as: "brand"
