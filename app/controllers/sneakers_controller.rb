bb1aa3638ec1f6d326cbff2d6b930ab55a9c4267
Remelson <rsmagatca@outlook.com>
2021-11-16 15:22:32 -0600

    Added search functionality

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,18 +1,35 @@
 class SneakersController < ApplicationController
   def index
     @sneakers = Sneaker.all
+    @brands = Brand.all
   end
 
   def show
     @sneakers = Sneaker.all
     @sneaker = Sneaker.find(params[:id])
+    @brands = Brand.all
   end
 
   def sale
     @sale = Sneaker.where("status LIKE ?", "sale")
+    @brands = Brand.all
   end
 
   def new
     @new = Sneaker.where("status LIKE ?", "new")
+    @brands = Brand.all
+  end
+
+  def search
+    @brands = Brand.all
+
+    wildcard_search = "%#{params[:keywords]}%"
+    brands = "#{params[:brands]}"
+
+    if params[:brands] == ""
+        @sneakers = Sneaker.where("name LIKE ?", wildcard_search)
+    else
+        @sneakers = Sneaker.where("name LIKE ? AND brand_id = ?", wildcard_search, brands)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
     root "sneakers#index"
-    get "/sneakers", to: "sneakers#index", as: "sneakers"
-    get "/sneakers/:id", to: "sneakers#show", as: "sneaker"
     get "/brands", to: "brands#index", as: "brands"
     get "/brands/:id", to: "brands#show", as: "brand"
     get "/sale", to: "sneakers#sale", as: "sale"
     get "/new", to: "sneakers#new", as: "new"
+
+    resources :sneakers, only: %i[index show] do
+        collection do
+            get "search"
+        end
+      end
 end
