0902c84b6222cf8b5768a234206daf61e8d914e4
Remelson <rsmagatca@outlook.com>
2021-11-18 17:29:09 -0600

    Added log-in and sign-up views for Customers (not generated yet)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     get "/brands/:id", to: "brands#show", as: "brand"
     get "/sale", to: "sneakers#sale", as: "sale"
     get "/new", to: "sneakers#new", as: "new"
+    get 'customers/index'
+    get 'customers/registration'
 
     resources :sneakers, only: %i[index show] do
         collection do
---
+++ test/controllers/customers_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class CustomersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customers_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get customers_create_url
+    assert_response :success
+  end
+end
