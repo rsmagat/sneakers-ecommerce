1afecb2bd8d2c496e4cb7544122a9c1220c84d5c
Renz <rsmagatca@outlook.com>
2021-12-09 22:15:33 -0600

    Re-deploying to Heroku

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
     belongs_to :province, optional: true
+    has_many :orders
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
